bb18fb6a833a2037a24fe2eac0216041fcf5ba70
Leo-Marin <leo.marin@etu.umontpellier.fr>
2020-10-15 13:24:42 +0200

    TD3 crypto 1.0.0

---
+++ TD3/cryptage.c
@@ -0,0 +1,29 @@
+//Includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+//Declarations
+
+
+
+
+
+//Implementation
+
+
+
+
+
+
+
+int main(){
+//test();
+	char* lecar = "salut";
+	printf("lecar = %s\n",lecar);
+	/*lecar[0] += 3;
+	printf("lecar = %s\n",lecar);*/
+	
+	//printf("lecar = %d\n",lecar);
+
+}
